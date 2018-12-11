9c8af0a93c6020f30af3364dc7c6ccb8bf08cbb9
João Barbosa <joao.paulo.barbosa@gmail.com>
2018-12-11 00:27:38 +0000

    qt: Remove ellipsis from sending/receiving addresses
    Considering https://stackoverflow.com/a/637708 the ellipsis in these
    menu actions should be removed.

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -364,9 +364,9 @@ void BitcoinGUI::createActions()
     openGraphAction->setEnabled(false);
     openPeersAction->setEnabled(false);
 
-    usedSendingAddressesAction = new QAction(QIcon(":/icons/" + theme + "/address-book"), tr("&Sending addresses..."), this);
+    usedSendingAddressesAction = new QAction(QIcon(":/icons/" + theme + "/address-book"), tr("&Sending addresses"), this);
     usedSendingAddressesAction->setStatusTip(tr("Show the list of used sending addresses and labels"));
-    usedReceivingAddressesAction = new QAction(QIcon(":/icons/" + theme + "/address-book"), tr("&Receiving addresses..."), this);
+    usedReceivingAddressesAction = new QAction(QIcon(":/icons/" + theme + "/address-book"), tr("&Receiving addresses"), this);
     usedReceivingAddressesAction->setStatusTip(tr("Show the list of used receiving addresses and labels"));
 
     openAction = new QAction(QIcon(":/icons/" + theme + "/open"), tr("Open $URI..."), this);

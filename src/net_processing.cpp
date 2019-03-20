51dab933bd89e92b1c5ff27d3bd1aaff6d8ef240
Pieter Wuille <pieter.wuille@gmail.com>
2019-03-19 18:06:35 -0700

    Simplify orphan processing in preparation for interruptibility
    Github-Pull: #15644
    Rebased-From: 9453018fdc8f02d42832374bcf1d6e3a1df02281

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -2514,6 +2514,8 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
             return true;
         }
 
+        std::set<uint256> orphan_work_set;
+
         CTransactionRef ptx;
         vRecv >> ptx;
         const CTransaction& tx = *ptx;
@@ -2542,7 +2544,7 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
                 auto it_by_prev = mapOrphanTransactionsByPrev.find(COutPoint(inv.hash, i));
                 if (it_by_prev != mapOrphanTransactionsByPrev.end()) {
                     for (const auto& elem : it_by_prev->second) {
-                        pfrom->orphan_work_set.insert(elem->first);
+                        orphan_work_set.insert(elem->first);
                     }
                 }
             }
@@ -2555,7 +2557,58 @@ bool static ProcessMessage(CNode* pfrom, const std::string& strCommand, CDataStr
                 mempool.size(), mempool.DynamicMemoryUsage() / 1000);
 
             // Recursively process any orphan transactions that depended on this one
-            ProcessOrphanTx(connman, pfrom->orphan_work_set, lRemovedTxn);
+            std::set<NodeId> setMisbehaving;
+            while (!orphan_work_set.empty()) {
+                const uint256 orphanHash = *orphan_work_set.begin();
+                orphan_work_set.erase(orphan_work_set.begin());
+
+                auto orphan_it = mapOrphanTransactions.find(orphanHash);
+                if (orphan_it == mapOrphanTransactions.end()) continue;
+
+                const CTransactionRef porphanTx = orphan_it->second.tx;
+                const CTransaction& orphanTx = *porphanTx;
+                NodeId fromPeer = orphan_it->second.fromPeer;
+                bool fMissingInputs2 = false;
+                // Use a dummy CValidationState so someone can't setup nodes to counter-DoS based on orphan
+                // resolution (that is, feeding people an invalid transaction based on LegitTxX in order to get
+                // anyone relaying LegitTxX banned)
+                CValidationState stateDummy;
+
+                if (setMisbehaving.count(fromPeer)) continue;
+                if (AcceptToMemoryPool(mempool, stateDummy, porphanTx, &fMissingInputs2, &lRemovedTxn, false /* bypass_limits */, 0 /* nAbsurdFee */)) {
+                    LogPrint(BCLog::MEMPOOL, "   accepted orphan tx %s\n", orphanHash.ToString());
+                    RelayTransaction(orphanTx, connman);
+                    for (unsigned int i = 0; i < orphanTx.vout.size(); i++) {
+                        auto it_by_prev = mapOrphanTransactionsByPrev.find(COutPoint(orphanHash, i));
+                        if (it_by_prev != mapOrphanTransactionsByPrev.end()) {
+                            for (const auto& elem : it_by_prev->second) {
+                                orphan_work_set.insert(elem->first);
+                            }
+                        }
+                    }
+                    EraseOrphanTx(orphanHash);
+                } else if (!fMissingInputs2) {
+                    int nDos = 0;
+                    if (stateDummy.IsInvalid(nDos) && nDos > 0) {
+                        // Punish peer that gave us an invalid orphan tx
+                        Misbehaving(fromPeer, nDos);
+                        setMisbehaving.insert(fromPeer);
+                        LogPrint(BCLog::MEMPOOL, "   invalid orphan tx %s\n", orphanHash.ToString());
+                    }
+                    // Has inputs but not accepted to mempool
+                    // Probably non-standard or insufficient fee
+                    LogPrint(BCLog::MEMPOOL, "   removed orphan tx %s\n", orphanHash.ToString());
+                    if (!orphanTx.HasWitness() && !stateDummy.CorruptionPossible()) {
+                        // Do not use rejection cache for witness transactions or
+                        // witness-stripped transactions, as they can have been malleated.
+                        // See https://github.com/bitcoin/bitcoin/issues/8279 for details.
+                        assert(recentRejects);
+                        recentRejects->insert(orphanHash);
+                    }
+                    EraseOrphanTx(orphanHash);
+                }
+                mempool.check(pcoinsTip.get());
+            }
         }
         else if (fMissingInputs)
         {

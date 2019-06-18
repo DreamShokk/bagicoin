83ff00035155adc51161abb269b441a0171d34c9
João Barbosa <joao.paulo.barbosa@gmail.com>
2019-06-18 14:13:03 +0100

    gui: Fix open wallet menu initialization order
    The menu must be created before connecting to aboutToShow signal.
    
    Github-Pull: #16231
    Rebased-From: 5224be5a3354e1a22ea4d7f0e40aadfccdf67064

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -158,6 +158,7 @@ private:
     QAction* showCoinJoinHelpAction = nullptr;
     QAction* proposalAction = nullptr;
     QAction* m_open_wallet_action{nullptr};
+    QMenu* m_open_wallet_menu{nullptr};
     QAction* m_close_wallet_action{nullptr};
     QAction* m_wallet_selector_label_action = nullptr;
     QAction* m_wallet_selector_action = nullptr;
